80dd3e25a85aac28ee66ba8046280b0ae480c996
Merill <merill@fr.fr>
2018-01-27 17:06:50 +0100

    cleaning & tuning
    - remove cout logs & "name" attribute
    - adjust %flow of smooth infill (90% +2*15% seems ok for my printer, less => gaps)

--- xs/src/libslic3r/Fill/Fill.cpp
+++ xs/src/libslic3r/Fill/Fill.cpp
@@ -252,7 +252,6 @@ void make_fill(LayerRegion &layerm, ExtrusionEntityCollection &out)
 			// Save into layer.
 			auto *eec = new ExtrusionEntityCollection();
 			out.entities.push_back(eec);
-			eec->name = "normal fill";
 			// Only concentric fills are not sorted.
 			eec->no_sort = f->no_sort();
 			extrusion_entities_append_paths(
@@ -273,7 +272,6 @@ void make_fill(LayerRegion &layerm, ExtrusionEntityCollection &out)
     // Why the paths are unpacked?
     for (const ExtrusionEntity *thin_fill : layerm.thin_fills.entities) {
         ExtrusionEntityCollection &collection = *(new ExtrusionEntityCollection());
-		collection.name="thin_wall";
         out.entities.push_back(&collection);
         collection.entities.push_back(thin_fill->clone());
     }
