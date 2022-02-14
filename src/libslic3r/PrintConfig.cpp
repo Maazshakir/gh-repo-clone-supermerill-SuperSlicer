supermerill/SuperSlicer

Added Client certificate authentication to physical printer options

src/libslic3r/PrintConfig.cpp
@@ -179,6 +179,13 @@ void PrintConfigDef::init_common_params()
     def->mode = comAdvanced;
     def->set_default_value(new ConfigOptionString(""));
 
+    def = this->add("printhost_client_cert", coString);
+    def->label = L("Client Certificate File");
+    def->category = OptionCategory::general;
+    def->tooltip = L("Custom Client certificate file can be specified for 2-way ssl authentication, in p12/pfx format. "
+                   "If left blank, now client certificate is used.");
+    def->mode = comAdvanced;
+    def->set_default_value(new ConfigOptionString(""));
 
     // Options used by physical printers
     
@@ -3021,6 +3028,13 @@ void PrintConfigDef::init_fff_params()
     def->label = L("HTTPS CA File");
     def->tooltip = L("Custom CA certificate file can be specified for HTTPS OctoPrint connections, in crt/pem format. "
                    "If left blank, the default OS CA certificate repository is used.");
+    def->mode = comAdvanced;
+    def->set_default_value(new ConfigOptionString(""));
+
+    def = this->add("printhost_client_cert", coString);
+    def->label = L("Client Certificate File");
+    def->tooltip = L("Custom Client certificate file can be specified for 2-way ssl authentication, in p12/pfx format. "
+                   "If left blank, now client certificate is used.");
     def->mode = comAdvanced;
     def->set_default_value(new ConfigOptionString(""));
 
@@ -6085,6 +6099,7 @@ std::unordered_set<std::string> prusa_export_to_remove_keys = {
 "print_temperature",
 "printhost_apikey",
 "printhost_cafile",
+"printhost_client_cert",
 "retract_lift_first_layer",
 "retract_lift_top",
 "seam_angle_cost",

src/slic3r/Utils/OctoPrint.hpp
@@ -32,6 +32,7 @@ public:
     std::string get_host() const override { return host; }
     const std::string& get_apikey() const { return apikey; }
     const std::string& get_cafile() const { return cafile; }
+    const std::string& get_client_cert() const { return client_cert; }
 
 protected:
     virtual bool validate_version_text(const boost::optional<std::string> &version_text) const;
@@ -40,6 +41,7 @@ private:
     std::string host;
     std::string apikey;
     std::string cafile;
+    std::string client_cert;
 
     virtual void set_auth(Http &http) const;
     std::string make_url(const std::string &path) const;

Maazshakir/gh-repo-clone-supermerill-SuperSlicer

src/libslic3r/PrintConfig.cpp
@@ -463,6 +463,13 @@ void PrintConfigDef::init_common_params()
     def->cli = ConfigOptionDef::nocli;
     def->set_default_value(new ConfigOptionString(""));
 
+    def = this->add("printhost_client_cert", coString);
+    def->label = L("Client Certificate File");
+    def->category = OptionCategory::general;
+    def->tooltip = L("Custom Client certificate file can be specified for 2-way ssl authentication, in p12/pfx format. "
+                   "If left blank, no client certificate is used.");
+    def->mode = comAdvanced;
+    def->set_default_value(new ConfigOptionString(""));
 
     // Options used by physical printers
     
@@ -3501,6 +3508,13 @@ void PrintConfigDef::init_fff_params()
     def->label = L("HTTPS CA File");
     def->tooltip = L("Custom CA certificate file can be specified for HTTPS OctoPrint connections, in crt/pem format. "
                    "If left blank, the default OS CA certificate repository is used.");
+    def->mode = comAdvanced;
+    def->set_default_value(new ConfigOptionString(""));
+
+    def = this->add("printhost_client_cert", coString);
+    def->label = L("Client Certificate File");
+    def->tooltip = L("Custom Client certificate file can be specified for 2-way ssl authentication, in p12/pfx format. "
+                   "If left blank, no client certificate is used.");
     def->mode = comAdvanced;
     def->set_default_value(new ConfigOptionString(""));
 
@@ -6983,6 +6997,7 @@ std::unordered_set<std::string> prusa_export_to_remove_keys = {
 "print_temperature",
 "printhost_apikey",
 "printhost_cafile",
+"printhost_client_cert",
 "retract_lift_first_layer",
 "retract_lift_top",
 "seam_angle_cost",

src/slic3r/Utils/OctoPrint.hpp
@@ -32,6 +32,7 @@ public:
     std::string get_host() const override { return m_host; }
     const std::string& get_apikey() const { return m_apikey; }
     const std::string& get_cafile() const { return m_cafile; }
+    const std::string& get_client_cert() const { return m_client_cert; }
 
 protected:
     virtual bool validate_version_text(const boost::optional<std::string> &version_text) const;
@@ -41,6 +42,7 @@ private:
     std::string m_apikey;
     std::string m_cafile;
     bool        m_ssl_revoke_best_effort;
+    std::string m_client_cert;
 
     virtual void set_auth(Http &http) const;
     std::string make_url(const std::string &path) const;